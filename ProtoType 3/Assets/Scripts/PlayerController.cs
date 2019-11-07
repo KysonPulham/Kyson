565b8d18407427a72ef96b83785392ffc2e2b9b1
Kyson <kyson.p401@stu.nebo.edu>
2019-11-07 10:29:59 -0700

    3.2 Part 2

--- ProtoType 3/Assets/Scripts/PlayerController.cs
+++ ProtoType 3/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float jumpForce;
+    public float jumpForce = 10;
     private Rigidbody playerRb;
     public float gravityModifier;
+    bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +18,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+      
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
---
+++ ProtoType 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
