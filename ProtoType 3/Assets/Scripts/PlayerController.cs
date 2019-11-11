416c9d498d3b6d87918a74c1308452004c7de318
Kyson <kyson.p401@stu.nebo.edu>
2019-11-11 10:08:15 -0700

    3.2
    Broke on Move.Left Needs Fixing

--- ProtoType 3/Assets/Scripts/PlayerController.cs
+++ ProtoType 3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float gravityModifier;
     bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +29,19 @@ public class PlayerController : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+            gameOver = true;
+            
+        }
+
+
+
+
     }
 }
--- ProtoType 3/Assets/Scripts/SpawnManager.cs
+++ ProtoType 3/Assets/Scripts/SpawnManager.cs
@@ -8,14 +8,19 @@ public class SpawnManager : MonoBehaviour
     private float repeatRate = 2;
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
-    
+    private PlayerController playerControllerScript;
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+        
     }
     
     void Update()
